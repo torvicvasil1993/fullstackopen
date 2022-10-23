67c5e521ba5eb19deba453953f3810a82de31bcb
Victor Silva <victorguidasilva@gmail.com>
2022-10-23 18:33:53 -0300

    1.6: unicafe step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log("Header",props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -38,27 +39,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>  
-      <Total  parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>  
+      <Total  parts={course.parts}/>
     </div>
   )
 }
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Display = ({ text, counter }) => <div> {text} {counter}</div>
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handleGoodClick = () => setGood(good + 1)
+  
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+
+  const handleBadClick = () => setBad(bad + 1)
+  
+
+  return (
+    <div>
+      <h2>Give feedback</h2>
+      <Button onClick={handleGoodClick} text="Good"/>
+      <Button onClick={handleNeutralClick} text="Neutral"/>
+      <Button onClick={handleBadClick} text="Bad"/>
+      <h2>Statistics</h2>
+      <Display text="good" counter={good}/>
+      <Display text="neutral" counter={neutral}/>
+      <Display text="bad" counter={bad}/>
+
+    </div>
+  )
+}
+
+export default App
