409666d81e55fb7d10ec056e800753eaf4b28a76
Victor Silva <victorguidasilva@gmail.com>
2022-10-23 19:58:06 -0300

    1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,22 +2,32 @@ import { useState } from 'react'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const StatisticLine = ({ text, counter }) => <div> {text} {counter}</div>
 
+/* https://github.com/facebook/react/issues/5652 */
+const StatisticLine = ({ text, counter }) => {
+  return (
+    <tbody>
+    <tr> 
+      <td>{text}</td> 
+      <td>{counter}</td>
+    </tr>
+  </tbody>
+  )
+}
 
 const Statistics = ( {good, neutral, bad} ) => {
   if (good === 0 && neutral === 0 && bad === 0) {
     return (<div> No feedback given </div>)
   }
   return (
-    <>
+    <table>
       <StatisticLine text="good" counter={good}/>
       <StatisticLine text="neutral" counter={neutral}/>
       <StatisticLine text="bad" counter={bad}/>
       <StatisticLine text="all" counter={good+neutral+bad}/>
       <StatisticLine text="average" counter={(good-bad)/(good+neutral+bad)}/>
       <StatisticLine text="positive" counter={(((good)/(good+neutral+bad))*(100))+"%"}/>
-    </>  
+    </table>  
   )
 }
 
