4914d387f3df6f307d343bcf09c89f12510a6403
Victor Silva <victorguidasilva@gmail.com>
2022-10-24 21:52:56 -0300

    made some corrections

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -20,9 +20,9 @@ const Content = (props) => {
 const Part = (props) => {
   console.log("Part",props)
   return(
-    <div>
+    <p>
       {props.part} {props.exercises}
-    </div>
+    </p>
   )
 }
 
@@ -36,37 +36,37 @@ const Total = (props) => {
 const Course = (props) => {
   return (
     <>
-      <Header name={props.name} />
-      <Content parts={props.parts}/>  
-      <Total  parts={props.parts}/>
+      <Header name={props.course.name} />
+      <Content parts={props.course.parts}/>  
+      <Total  parts={props.course.parts}/>
     </>
   )
 }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Course name={course.name} parts={course.parts}/>
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
